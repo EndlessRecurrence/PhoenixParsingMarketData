5a81c991f87591208a686ae70f8d5a92904c04a2
EndlessRecurrence <georgesut@yahoo.com>
2023-03-29 12:16:02 +0300

    Add light blue background on the edit/create form

--- lib/phoenix_parsing_market_data_web/components/core_components.ex
+++ lib/phoenix_parsing_market_data_web/components/core_components.ex
@@ -227,7 +227,7 @@ defmodule PhoenixParsingMarketDataWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="space-y-8 bg-white mt-10">
+      <div class="space-y-8 bg-blue-100 mt-10">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
